1b6efc7194c7e6a5070fe1dea69242f270dbda1e
Patey0911 <mihalybogdanmatei@yahoo.com>
2020-06-12 19:06:34 +0300

    Second_commit

--- Face_Detection-master/Detection/Detection.py
+++ Face_Detection-master/Detection/Detection.py
@@ -1,15 +1,23 @@
 import cv2
 
+eyes_cascade=cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap=cv2.VideoCapture(0)
 _,frame1=cap.read()
+
 while(True):
-    imgGrey = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(imgGrey, 1.1, 4)
+    gray = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for(x, y, w, h) in faces:
         cv2.rectangle(frame1, (x,y), (x+w,y+h), (255, 0, 0), 2)
+        roi_gray=gray[y:y+h, x:x+w]
+        roi_color=frame1[y:y+h, x:x+w]
+        eyes=eyes_cascade.detectMultiScale(roi_gray)
+        for(ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,0,255), 2)
+
 
     cv2.imshow('window', frame1)
     _,frame1=cap.read()
